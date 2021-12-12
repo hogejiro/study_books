e5cefe0e7701ec0920c6384bf1da43ea4945d19c
hogejiro <1515179+hogejiro@users.noreply.github.com>
2021-12-13 00:46:29 +0900

    add part of the LoL chap15 (optimization)

--- land_of_lisp/15-dice-of-doom-a-game-written-in-the-functional-style/dice-of-doom.lisp
+++ land_of_lisp/15-dice-of-doom-a-game-written-in-the-functional-style/dice-of-doom.lisp
@@ -1,6 +1,6 @@
 (defparameter *num-players* 2)
 (defparameter *max-dice* 3)
-(defparameter *board-size* 2)
+(defparameter *board-size* 3)
 (defparameter *board-hexnum* (* *board-size* *board-size*))
 
 (defun board-array (lst)
@@ -92,6 +92,7 @@
                                    ((eq pos dst) (list player (1- dice)))
                                    (t hex)))))
 
+#|
 ; (add-new-dice #((0 1) (1 3) (0 2) (1 1)) 0 2)
 ; #((0 2) (1 3) (0 3) (1 1))
 (defun add-new-dice (board player spare-dice)
@@ -105,6 +106,7 @@
                                   (f (cdr lst) (1- n)))
                             (cons (car lst) (f (cdr lst) n))))))))
            (board-array (f (coerce board 'list) spare-dice))))
+|#
 
 (defun play-vs-human (tree)
   (print-info tree)
@@ -172,8 +174,39 @@
   (let ((ratings (get-ratings tree (car tree))))
     (cadr (nth (position (apply #'max ratings) ratings) (caddr tree)))))
 
+; (play-vs-computer (game-tree (gen-board) 0 0 t)
 (defun play-vs-computer (tree)
   (print-info tree)
   (cond ((null (caddr tree)) (announce-winner (cadr tree)))
       ((zerop (car tree)) (play-vs-computer (handle-human tree)))
       (t (play-vs-computer (handle-computer tree)))))
+
+(let ((old-game-tree (symbol-function 'game-tree))
+      (previous (make-hash-table :test #'equalp)))
+  (defun game-tree (&rest rest)
+    (or (gethash rest previous)
+      (setf (gethash rest previous) (apply old-game-tree rest)))))
+
+(let ((old-rate-position (symbol-function 'rate-position))
+      (previous (make-hash-table)))
+  (defun rate-position (tree player)
+    (let ((tab (gethash player previous)))
+      (unless tab
+        (setf tab (setf (gethash player previous) (make-hash-table))))
+      (or (gethash tree tab)
+          (setf (gethash tree tab)
+                (funcall old-rate-position tree player))))))
+
+(defun add-new-dice (board player spare-dice)
+  (labels ((f (lst n acc)
+             (cond ((zerop n) (append (reverse acc) lst))
+                   ((null lst) (reverse acc))
+                   (t (let ((cur-player (caar lst))
+                            (cur-dice (cadar lst)))
+                        (if (and (eq cur-player player)
+                                 (< cur-dice *max-dice*))
+                            (f (cdr lst)
+                               (1- n)
+                               (cons (list cur-player (1+ cur-dice)) acc))
+                          (f (cdr lst) n (cons (car lst) acc))))))))
+    (board-array (f (coerce board 'list) spare-dice ()))))
