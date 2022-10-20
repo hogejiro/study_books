80ce140a62c4f9c33d8540e673e423a1b2810d59
hogejiro <1515179+hogejiro@users.noreply.github.com>
2022-10-21 00:00:09 +0900

    csl03: chap 8

--- computer_science_library_03/chap08/problem.ml
+++ computer_science_library_03/chap08/problem.ml
@@ -25,12 +25,54 @@ let pen = { purchased_item = "アップルペンシル"; purchased_price = 25800
 
 (* 8.3 *)
 type person_t = {
+  namae : string ;
   shincho : float ;
   taiju : float ;
   tanjoubi : int * int ;
   ketsuekigata : string ;
 }
 
-let fuga = { shincho = 170.; taiju = 50.; tanjoubi = (1, 1); ketsuekigata = "A" }
-let hoge = { shincho = 148.; taiju = 45.; tanjoubi = (6, 6); ketsuekigata = "O" }
-let foo = { shincho = 190.; taiju = 80.; tanjoubi = (12, 12); ketsuekigata = "B" }
+let fuga = { namae = "fuga"; shincho = 170.; taiju = 50.; tanjoubi = (1, 1); ketsuekigata = "A" }
+let hoge = { namae = "hoge"; shincho = 148.; taiju = 45.; tanjoubi = (6, 6); ketsuekigata = "O" }
+let foo = { namae = "foo"; shincho = 190.; taiju = 80.; tanjoubi = (12, 12); ketsuekigata = "B" }
+
+(* 8.4 *)
+(* person_t 型のデータを受け取ったら「○○さんの血液型は△型です」という文字列を返す *)
+(* ketsueki_hyoji: person_t -> string *)
+let ketsueki_hyoji person = match person with
+  {namae = n; shincho = s; taiju = ta; tanjoubi = tb; ketsuekigata = k} -> n ^ "さんの血液型は" ^ k ^ "型です"
+
+let kt_test1 = ketsueki_hyoji fuga = "fugaさんの血液型はA型です"
+let kt_test2 = ketsueki_hyoji hoge = "hogeさんの血液型はO型です"
+let kt_test3 = ketsueki_hyoji foo = "fooさんの血液型はB型です"
+
+(* 8.5 *)
+type ekimei_t = {
+  kanji : string ;
+  kana : string ;
+  romaji : string ;
+  shozoku : string ;
+}
+
+(* 8.6 *)
+(* ekimei_t 型のデータを受け取ったら「路線名, 駅名（かな）」という文字列を返す *)
+(* hyoji: ekimei_t -> string *)
+let hyoji ekimei = match ekimei with
+  {kanji = kj; kana = kn; romaji = rj; shozoku = sz} -> sz ^ ", " ^ kj ^ "（" ^ kn ^ "）"
+
+let myogadani = {kanji = "茗荷谷"; kana = "みょうがだに"; romaji = "myogadani"; shozoku = "丸ノ内線" }
+let shinkoiwa = {kanji = "新小岩"; kana = "しんこいわ"; romaji = "shinkoiwa"; shozoku = "総武線" }
+let azabujuban = {kanji = "麻布十番"; kana = "あざぶじゅうばん"; romaji = "azabujuban"; shozoku = "大江戸線" }
+
+let hyoji_test1 = hyoji myogadani = "丸ノ内線, 茗荷谷（みょうがだに）"
+let hyoji_test2 = hyoji shinkoiwa = "総武線, 新小岩（しんこいわ）"
+let hyoji_test3 = hyoji azabujuban = "大江戸線, 麻布十番（あざぶじゅうばん）"
+
+(* 8.7 *)
+type ekikan_t = {
+  kiten : string ;
+  shuten : string ;
+  keiyu : string ;
+  kyori : float ;
+  jikan : int ;
+}
